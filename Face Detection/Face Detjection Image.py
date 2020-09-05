da0dd5af9f8b96b0470ce026ed281af09eacedba
Ankit4371 <67886323+Ankit4371@users.noreply.github.com>
2020-09-05 13:53:54 +0530

    Update Face Detjection Image.py

--- Face Detection/Face Detjection Image.py
+++ Face Detection/Face Detjection Image.py
@@ -1,67 +1,33 @@
-# USAGE
-# python detect_faces.py --image rooster.jpg --prototxt deploy.prototxt.txt --model res10_300x300_ssd_iter_140000.caffemodel
-
-# import the necessary packages
 import numpy as np
-# import argparse
 import cv2
 
-#
-# # construct the argument parse and parse the arguments
-# ap = argparse.ArgumentParser()
-# ap.add_argument("-i", "--image", required=True,
-# 	help="path to input image")
-# ap.add_argument("-p", "--prototxt", required=True,
-# 	help="path to Caffe 'deploy' prototxt file")
-# ap.add_argument("-m", "--model", required=True,
-# 	help="path to Caffe pre-trained model")
-# ap.add_argument("-c", "--confidence", type=float, default=0.5,
-# 	help="minimum probability to filter weak detections")
-# args = vars(ap.parse_args())
-
-# load our serialized model from disk
-print("[INFO] loading model...")
-prototxt_path = "deploy.prototxt.txt"
-model_path = "res10_300x300_ssd_iter_140000.caffemodel"
+prototxt_path = ""
+model_path = ""
 model = cv2.dnn.readNetFromCaffe(prototxt_path, model_path)
 
-# load the input image and construct an input blob for the image
-# by resizing to a fixed 300x300 pixels and then normalizing it
-image = cv2.imread("rooster.jpg")
+image = cv2.imread(" ")
 (h, w) = image.shape[:2]
 blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0,
                              (300, 300), (104.0, 177.0, 123.0))
-
-# pass the blob through the network and obtain the detections and
-# predictions
-print("[INFO] computing object detections...")
 model.setInput(blob)
 detections = np.squeeze(model.forward())
 
-# loop over the detections
 for i in range(0, detections.shape[0]):
-    # extract the confidence (i.e., probability) associated with the
-    # prediction
+ 
     confidence = detections[i, 2]
-
-    # filter out weak detections by ensuring the `confidence` is
-    # greater than the minimum confidence
+  
     if confidence > 0.5:
-        # compute the (x, y)-coordinates of the bounding box for the
-        # object
+       
         box = detections[i, 3:7] * np.array([w, h, w, h])
         (startX, startY, endX, endY) = box.astype(np.int)
-
-        # draw the bounding box of the face along with the associated
-        # probability
-        text = "{:.2f}%".format(confidence * 100)
+        
+        probability = "{:.2f}%".format(confidence * 100)
         y = startY - 10 if startY - 10 > 10 else startY + 10
         cv2.rectangle(image, (startX, startY), (endX, endY),
-                      (0, 0, 255), 2)
-        cv2.putText(image, text, (startX, y),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+                      (255, 255, 255), 2)
+        cv2.putText(image, probability, (startX, y),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 255), 2)
 
-# show the output image
 cv2.imshow("Output", image)
 cv2.waitKey(0)
 cv2.imwrite("detected image",image)
